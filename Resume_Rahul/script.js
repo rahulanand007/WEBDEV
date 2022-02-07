c44898111ab23392717ea68a4efabdc5d2901cdc
rahulanand007 <rahul03061998@gmail.com>
2022-02-07 18:40:48 +0530

    commit

--- Resume_Rahul/script.js
+++ Resume_Rahul/script.js
@@ -12,7 +12,9 @@ document.querySelector('.ham').addEventListener("click",()=>{
 document.querySelector('.cross').addEventListener("click",()=>{
     document.querySelector('.sidebar').classList.toggle('sidebarGO');
         if(document.querySelector('.sidebar').classList.contains('sidebarGO')){
+            setTimeout(()=>{
             document.querySelector('.ham').style.display='inline'
+            },300)
         }else{
             document.querySelector('.ham').style.display= 'none'
         }
