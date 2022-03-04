3e018245b06c69aa3849cf075a2401d18ec60738
rahulanand007 <rahul03061998@gmail.com>
2022-03-04 11:37:43 +0530

    commit

---
+++ React/to_do_list/src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h3>This is my about component</h3>
+        <p>dhsfv hewbfdhjud hebdihebdkhbdebkhbid yebfihebkhd bc iefbiwuend ewfbenfoiend ewjnbwkjendkjbdiehyvdiyewbkjednb ibfdiuweibefdhebfdiweubiweubcoiweuci
+        </p>
+    </div>
+  )
+}
