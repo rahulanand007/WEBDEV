a97efc383036a199bf87e179197e3df1b8657dab
rahulanand007 <rahul03061998@gmail.com>
2022-04-13 20:20:26 +0530

    commit

--- React/cloudnotebook/BACKEND/routes/auth.js
+++ React/cloudnotebook/BACKEND/routes/auth.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 // Create a user using: POST "/api/auth/". Doesn't require Auth
-router.post('/', (req,res)=>{
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send(req.body) 
+router.post('/', [
+    body('name', "Name length should be a minimum of 3 characters").isLength({min:3}),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password should not be less than 5 characters').isLength({min:5}),
+] ,(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+      }).then(user => res.json(user))
+      .catch(err=>{console.log(err)
+      res.json({error: 'Email already taken! Please enter a different value'})})
+    
 })
 
 module.exports = router
